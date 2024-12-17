8ed3da91e7b92d96d3c2172e72b3067a1dcbc07d
ESSantana <emersonsantana.dev@gmail.com>
2024-12-17 19:46:46 -0300

    testing modules

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,8 @@ variable aws_region {
   default     = "sa-east-1"
   description = "resource aws region"
 }
+
+variable stage {    
+  type        = string
+  description = "Stage (environment) of the workspace"
+}
