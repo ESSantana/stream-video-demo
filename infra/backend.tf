dfd1b01802199ef5d7a6b274f21b871895831043
ESSantana <emersonsantana.dev@gmail.com>
2024-12-17 20:36:06 -0300

    remove action

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,17 @@
 terraform {
   backend "s3" {}
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      region = var.aws_region
+    }
+
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.7.0"
+    }
+     
+  }
+  
 }
