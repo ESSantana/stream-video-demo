2f8c2c3a69a641af82acd9a572de7f051456e052
ESSantana <emersonsantana.dev@gmail.com>
2024-12-27 17:39:13 -0300

    recreate api

--- infra/api.tf
+++ infra/api.tf
@@ -4,49 +4,43 @@ resource "aws_api_gateway_rest_api" "stream_video_api" {
 }
 
 resource "aws_api_gateway_resource" "stream_video_resource" {
-  parent_id   = aws_api_gateway_rest_api.stream_video_api.root_resource_id
-  path_part   = "stream"
-  rest_api_id = aws_api_gateway_rest_api.stream_video_api.id
-}
-
-resource "aws_api_gateway_resource" "upload_video_resource" {
   parent_id   = aws_api_gateway_resource.stream_video_resource.id
-  path_part   = "upload"
+  path_part   = "{proxy+}"
   rest_api_id = aws_api_gateway_rest_api.stream_video_api.id
 }
 
-resource "aws_api_gateway_method" "upload_video_method" {
+resource "aws_api_gateway_method" "stream_video_method" {
   authorization = "NONE"
-  http_method   = "POST"
-  resource_id   = aws_api_gateway_resource.upload_video_resource.id
+  http_method   = "ANY"
+  resource_id   = aws_api_gateway_resource.stream_video_resource.id
   rest_api_id   = aws_api_gateway_rest_api.stream_video_api.id
 }
 
-resource "aws_api_gateway_integration" "upload_video_integration" {
-  http_method             = aws_api_gateway_method.upload_video_method.http_method
-  resource_id             = aws_api_gateway_resource.upload_video_resource.id
+resource "aws_api_gateway_integration" "stream_video_integration" {
+  http_method             = aws_api_gateway_method.stream_video_method.http_method
+  resource_id             = aws_api_gateway_resource.stream_video_resource.id
   rest_api_id             = aws_api_gateway_rest_api.stream_video_api.id
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri                     = module.upload_video.lambda_invoke_arn
+  uri                     = module.stream-video-api.lambda_invoke_arn
 }
 
-resource "aws_lambda_permission" "upload_video_lambda_permission" {
+resource "aws_lambda_permission" "stream_video_lambda_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = module.upload_video.lambda_name
+  function_name = module.stream_video.lambda_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.accountId}:${aws_api_gateway_rest_api.stream_video_api.id}/*/${aws_api_gateway_method.upload_video_method.http_method}${aws_api_gateway_resource.upload_video_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.accountId}:${aws_api_gateway_rest_api.stream_video_api.id}/*/${aws_api_gateway_method.stream_video_method.http_method}${aws_api_gateway_resource.stream_video_resource.path}"
 }
 
-resource "aws_api_gateway_deployment" "upload_video_deployment" {
+resource "aws_api_gateway_deployment" "stream_video_deployment" {
   rest_api_id = aws_api_gateway_rest_api.stream_video_api.id
 
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.upload_video_resource.id,
-      aws_api_gateway_method.upload_video_method.id,
-      aws_api_gateway_integration.upload_video_integration.id,
+      aws_api_gateway_resource.stream_video_resource.id,
+      aws_api_gateway_method.stream_video_method.id,
+      aws_api_gateway_integration.stream_video_integration.id,
     ]))
   }
 
@@ -56,7 +50,7 @@ resource "aws_api_gateway_deployment" "upload_video_deployment" {
 }
 
 resource "aws_api_gateway_stage" "stream_video_stage" {
-  deployment_id = aws_api_gateway_deployment.upload_video_deployment.id
+  deployment_id = aws_api_gateway_deployment.stream_video_deployment.id
   rest_api_id   = aws_api_gateway_rest_api.stream_video_api.id
   stage_name    = var.stage
 }
