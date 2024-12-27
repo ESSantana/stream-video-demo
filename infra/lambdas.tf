ebf5e6566f4aac855f4f55a882f2f7e778250963
ESSantana <emersonsantana.dev@gmail.com>
2024-12-27 17:35:39 -0300

    recreating lambda and apis

--- infra/lambdas.tf
+++ infra/lambdas.tf
@@ -1,29 +1,29 @@
-# module "stream-video-api" { 
-#     source = "./modules/lambda-function"
+module "stream-video-api" { 
+    source = "./modules/lambda-function"
 
-#     function_name = "stream-video-api"
-#     stage         = var.stage
-#     handler       = "bin/api/boostrap" 
-#     environment_variables = {
-#       VIDEO_BUCKET = "teste-lambda-vars-${var.stage}"
-#     }
+    function_name = "stream-video-api"
+    stage         = var.stage
+    handler       = "bin/api/boostrap" 
+    environment_variables = {
+      VIDEO_BUCKET = "teste-lambda-vars-${var.stage}"
+    }
 
-#     tags = {
-#       STAGE = var.stage
-#     }
-# }
+    tags = {
+      STAGE = var.stage
+    }
+}
 
-# data "aws_iam_policy_document" "stream_video_policy_document" {
-#   statement {
-#     effect    = "Allow"
-#     actions   = ["s3:*"]
-#     resources = ["*"]
-#   }
-# }
+data "aws_iam_policy_document" "stream_video_policy_document" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:*"]
+    resources = ["*"]
+  }
+}
 
-# resource "aws_iam_role_policy" "stream_video_role_policy" {
-#   name    = "stream-video-role-policy"
-#   role    = module.stream-video-api.lambda_role_id
-#   policy  = data.aws_iam_policy_document.stream_video_policy_document.json
-# }
+resource "aws_iam_role_policy" "stream_video_role_policy" {
+  name    = "stream-video-role-policy"
+  role    = module.stream-video-api.lambda_role_id
+  policy  = data.aws_iam_policy_document.stream_video_policy_document.json
+}
 
