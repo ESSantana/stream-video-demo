2fd06dc7d60dea5dfe587ae8b80b4f0f4ad0a7d6
ESSantana <emersonsantana.dev@gmail.com>
2024-12-27 18:43:53 -0300

    vaio

--- infra/api.tf
+++ infra/api.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_method" "stream_video_method" {
 
 resource "aws_api_gateway_integration" "stream_video_integration" {
   http_method             = aws_api_gateway_method.stream_video_method.http_method
-  resource_id             = aws_api_gateway_resource.stream_video_resource.id
+  resource_id             = aws_api_gateway_method.stream_video_method.resource_id
   rest_api_id             = aws_api_gateway_rest_api.stream_video_api.id
   type                    = "AWS_PROXY"
   integration_http_method = "POST"
@@ -30,7 +30,7 @@ resource "aws_lambda_permission" "stream_video_lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name = module.stream_video_api.lambda_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.accountId}:${aws_api_gateway_rest_api.stream_video_api.id}/*/${aws_api_gateway_method.stream_video_method.http_method}${aws_api_gateway_resource.stream_video_resource.path}"
+  source_arn = "${aws_api_gateway_rest_api.stream_video_api.execution_arn}/*/*"
 }
 
 resource "aws_api_gateway_deployment" "stream_video_deployment" {
