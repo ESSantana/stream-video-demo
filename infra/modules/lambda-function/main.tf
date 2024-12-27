e3e41c9c7285f5b1ed2ede936410e86dd7abf594
ESSantana <emersonsantana.dev@gmail.com>
2024-12-27 19:25:36 -0300

    sla

--- infra/modules/lambda-function/main.tf
+++ infra/modules/lambda-function/main.tf
@@ -65,6 +65,7 @@ resource "aws_lambda_function" "lambda" {
   runtime       = var.runtime
   memory_size   = var.memory_size
   timeout       = var.timeout
+  architecture  = var.architecture
 
   role          = aws_iam_role.iam_for_lambda.arn
 
