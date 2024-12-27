03ea53aa1862bedc9991d866ed662e04430fc9cb
ESSantana <emersonsantana.dev@gmail.com>
2024-12-27 17:41:32 -0300

    recreate api

--- infra/api.tf
+++ infra/api.tf
@@ -4,7 +4,7 @@ resource "aws_api_gateway_rest_api" "stream_video_api" {
 }
 
 resource "aws_api_gateway_resource" "stream_video_resource" {
-  parent_id   = aws_api_gateway_resource.stream_video_resource.id
+  parent_id   = aws_api_gateway_rest_api.stream_video_api.root_resource_id
   path_part   = "{proxy+}"
   rest_api_id = aws_api_gateway_rest_api.stream_video_api.id
 }
