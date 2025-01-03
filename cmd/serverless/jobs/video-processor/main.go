e5b2dc309d8bf0572ac8bf26abe07481d338ac26
ESSantana <emersonsantana.dev@gmail.com>
2025-01-02 21:20:01 -0300

    using zerologger

--- cmd/serverless/jobs/video-processor/main.go
+++ cmd/serverless/jobs/video-processor/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -30,13 +32,17 @@ func main() {
 }
 
 func Handler(ctx context.Context, event events.S3Event) error {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	log.Print("hello world")
+
 	out, err := json.Marshal(event)
 	if err != nil {
-		fmt.Printf("Error marshalling event: %s\n", err.Error())
+		log.Printf("Error marshalling event: %s\n", err.Error())
 		return nil
 	}
 
-	fmt.Printf("Processing event: %s\n", out)
+	log.Printf("Processing event: %s\n", out)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,12 @@ go 1.23.2
 
 require github.com/go-chi/chi/v5 v5.1.0
 
-require golang.org/x/net v0.31.0 // indirect
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+)
 
 require (
 	github.com/aws/aws-lambda-go v1.47.0
@@ -12,6 +17,7 @@ require (
 	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
 	github.com/google/uuid v1.6.0
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/rs/zerolog v1.33.0
 	github.com/u2takey/ffmpeg-go v0.5.0
 	github.com/u2takey/go-utils v0.3.1 // indirect
 )
