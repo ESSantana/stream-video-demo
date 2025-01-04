a2cc0d35d0235ca601352f2ce09b5688cd7a89ec
ESSantana <emersonsantana.dev@gmail.com>
2025-01-04 01:53:48 -0300

    updating infra

--- infra/server.tf
+++ infra/server.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "server" {
   instance_type           = "t2.micro"
   user_data = file("./scripts/server_user_data.sh")
   vpc_security_group_ids  = [aws_security_group.server_security_group.id]
+  key_name                = "aws-emerson-sa-east-1"
 }
 
 resource "aws_security_group" "server_security_group" {
@@ -12,6 +13,13 @@ resource "aws_security_group" "server_security_group" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
