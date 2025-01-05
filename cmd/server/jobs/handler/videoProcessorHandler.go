e6255c28618e04ead14120181ac99aa1aeccb10d
ESSantana <emersonsantana.dev@gmail.com>
2025-01-05 00:37:04 -0300

    parsing sns event

--- cmd/server/jobs/handler/videoProcessorHandler.go
+++ cmd/server/jobs/handler/videoProcessorHandler.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+
 	// "os"
 
 	"github.com/ESSantana/streaming-test/internal/services/interfaces"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/rs/zerolog/log"
 )
 
@@ -25,6 +28,21 @@ func (h *VideoProcessorHandler) ProcessVideo(w http.ResponseWriter, r *http.Requ
 	}
 	defer r.Body.Close()
 
+	var snsMessage events.SNSEntity
+	err = json.Unmarshal(data, &snsMessage)
+	if err != nil {
+		log.Error().Msg(err.Error())
+		return
+	}
+
+	var s3Events events.S3Event
+	err = json.Unmarshal([]byte(snsMessage.Message), &s3Events)
+	if err != nil {
+		log.Error().Msg(err.Error())
+		return
+	}
+
+	log.Info().Msg(s3Events.Records[0].S3.Object.Key)
 	// go func() {
 	// 	err = h.videoService.ProcessVideoWithOptions(r.Context(), os.Getenv("VIDEO_BUCKET"), "raw/epic_sax_guy.mp4", nil)
 	// 	if err != nil {
