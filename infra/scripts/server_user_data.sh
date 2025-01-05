7225414acfa7c314b94a42febd166abfa109a1ef
ESSantana <emersonsantana.dev@gmail.com>
2025-01-04 23:25:21 -0300

    add docker deploy github action

--- infra/scripts/server_user_data.sh
+++ infra/scripts/server_user_data.sh
@@ -6,7 +6,7 @@ yum install -y docker
 systemctl start docker.service
 usermod -a -G docker ec2-user
 
-docker run -p 80:8080 -d --name video-streaming-demo \
+docker run -p 80:8080 -d --name video-streaming-server \
     -e SERVER_PORT="8080" \
     -e VIDEO_BUCKET="essantana-videos-sa-east-1-production" \
-    emersonsantanadev/video-streaming-demo:latest 
+    emersonsantanadev/video-streaming-server:latest 
