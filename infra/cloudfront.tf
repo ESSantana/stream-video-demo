97208bb9893ce17ffac7db2ea5568f407ddaf3fe
Emerson Santana <emerson.santana@acordocerto.com.br>
2025-01-07 03:49:01 -0300

    refactor infra and add cloudfront

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -16,9 +16,9 @@ resource "aws_cloudfront_distribution" "video_stream_distribution" {
     allowed_methods           = ["GET", "HEAD", "OPTIONS"]
     cached_methods            = ["GET", "HEAD"]
     compress                  = true
-    cache_policy_id           = "Managed-CachingOptimized"
+    cache_policy_id           = data.aws_cloudfront_cache_policy.cache_policy.id
     viewer_protocol_policy    = "allow-all"
-    origin_request_policy_id  = "Managed-CORS-CustomOrigin"
+    origin_request_policy_id  = data.aws_cloudfront_origin_request_policy.origin_request_policy.id
   }
 
   price_class = "PriceClass_200"
@@ -41,4 +41,12 @@ resource "aws_cloudfront_distribution" "video_stream_distribution" {
 
 resource "aws_cloudfront_origin_access_identity" "stream_video_distribution_oai" {
   comment = "OAI to access S3 stream video bucket"
+}
+
+data "aws_cloudfront_cache_policy" "cache_policy" {
+  name = "Managed-CachingOptimized"
+}
+
+data "aws_cloudfront_origin_request_policy" "origin_request_policy" {
+  name = "Managed-CORS-CustomOrigin"
 }
