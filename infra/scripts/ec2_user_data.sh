3213e4f89445497758f41519a53b5c1b42622183
Emerson Santana <emersonsantana.dev@gmail.com>
2025-05-18 02:41:43 -0300

    testing new ec2 start script

--- infra/scripts/ec2_user_data.sh
+++ infra/scripts/ec2_user_data.sh
@@ -7,9 +7,17 @@ systemctl enable docker
 systemctl start docker.service
 usermod -a -G docker ec2-user
 
+export VIDEO_BUCKET=${VIDEO_BUCKET}
+export CLOUDFRONT_DIST=${CLOUDFRONT_DIST}
+export STAGE=${STAGE}
+export SERVER_PORT=${SERVER_PORT}
+
+source /etc/bashrc
+
 docker run -p 80:${SERVER_PORT} -d --name stream-video-demo \
     -e SERVER_PORT \
     -e VIDEO_BUCKET \
     -e CLOUDFRONT_DIST \
+    -e STAGE \
     emersonsantanadev/stream-video-demo:latest
  
